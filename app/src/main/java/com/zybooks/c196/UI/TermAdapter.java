6ae9a059dbb5212b051716228f81981d01d7a690
ceciliaconsta3 <ceciliaconsta3@gmail.com>
2022-04-11 23:59:33 -0400

    :construction: setting up itemviewholder

--- app/src/main/java/com/zybooks/c196/UI/TermAdapter.java
+++ app/src/main/java/com/zybooks/c196/UI/TermAdapter.java
@@ -1,6 +1,10 @@
 package com.zybooks.c196.UI;
 
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zybooks.c196.R;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -8,6 +12,20 @@ import androidx.recyclerview.widget.RecyclerView;
 
 public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder>{
 
+    // viewholder tells the adapter everything in each list item
+    class TermViewHolder extends RecyclerView.ViewHolder{
+        private final TextView termItemView;
+        private TermViewHolder(View itemView){ // this is the constructor
+            super(itemView);
+            termItemView = itemView.findViewById(R.id.textView2);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    
+                }
+            });
+        }
+    }
 
     @NonNull
     @Override
