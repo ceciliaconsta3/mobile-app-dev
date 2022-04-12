9871e3093796c0f857a11defa4c578b3ef247875
ceciliaconsta3 <ceciliaconsta3@gmail.com>
2022-04-11 23:52:22 -0400

    :construction: begin add termadapter and view holder via cohort 3 implementation

---
+++ app/src/main/java/com/zybooks/c196/UI/TermAdapter.java
@@ -0,0 +1,27 @@
+package com.zybooks.c196.UI;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+public class TermAdapter extends RecyclerView.Adapter<TermAdapter.TermViewHolder>{
+
+
+    @NonNull
+    @Override
+    public TermAdapter.TermViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermAdapter.TermViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
